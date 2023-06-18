92ebf0eabdc5eeb4d1ede9853695d6ad2b9862a2
Carlos Faria <carlosfaria89@gmail.com>
2023-06-18 13:48:11 +0100

    Add initial config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- In visual mode, allows selected text to be moved top or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep yanked value even after pasting it over something
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Replace the all the words similar to the word on top of my cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
---
+++ lua/plugins/monokai.lua
@@ -0,0 +1,10 @@
+return {
+  -- monokai-pro theme
+  { "loctvl842/monokai-pro.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+}
